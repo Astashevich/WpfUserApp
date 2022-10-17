4d6ac47fe97b50e86252cc80e457ee5b1e9ac074
Arseniy Astashevich <ars.astashevich@gmail.com>
2022-10-17 17:16:37 +0300

    Creating a SQLite Database

---
+++ WpfUserApp/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace WpfUserApp
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public ApplicationDbContext() : base("DefaultConnection")
+        {
+
+        }
+    }
+}
---
+++ WpfUserApp/User.cs
@@ -0,0 +1,19 @@
+﻿namespace WpfUserApp
+{
+    public class User
+    {
+        private int id { get; set; }
+        private string login { get; set; }
+        private string password { get; set; }
+        private string email { get; set; }
+
+        public User() { }
+
+        public User(string login, string email, string password)
+        {
+            this.login = login;
+            this.email = email;
+            this.password = password;
+        }
+    }
+}
