d6822e02de76103a0d6b8831f4338944e69024a8
Arseniy Astashevich <ars.astashevich@gmail.com>
2022-10-17 16:49:30 +0300

    Receiving data. Bug tracking

--- WpfUserApp/MainWindow.xaml.cs
+++ WpfUserApp/MainWindow.xaml.cs
@@ -24,5 +24,47 @@ namespace WpfUserApp
         {
             InitializeComponent();
         }
+
+        private void ButtonRegister_Click(object sender, RoutedEventArgs e)
+        {
+            string login = textBoxLogin.Text.Trim();
+            string password = passwordBox.Password.Trim();
+            string password2 = passwordBox_2.Password.Trim();
+            string email = textBoxEmail.Text.ToLower().Trim();
+
+            if (login.Length < 5)
+            {
+                textBoxLogin.ToolTip = "Minimum login length 5 characters.";
+                textBoxLogin.Background = Brushes.DarkRed;
+            }
+            else if(password.Length < 5)
+            {
+                passwordBox.ToolTip = "Minimum password length 5 characters.";
+                passwordBox.Background = Brushes.DarkRed;
+            }
+            else if(password != password2)
+            {
+                passwordBox_2.ToolTip = "Passwords are not equal";
+                passwordBox_2.Background = Brushes.DarkRed;
+            }
+            else if(email.Length < 5 || !email.Contains("@") || !email.Contains(".")) 
+            {
+                textBoxEmail.ToolTip = "Minimum email length 5 characters or incorect email.";
+                textBoxEmail.Background = Brushes.DarkRed;
+            }
+            else
+            {
+                textBoxLogin.ToolTip = "";
+                textBoxLogin.Background = Brushes.Transparent;
+                passwordBox.ToolTip = "";
+                passwordBox.Background = Brushes.Transparent;
+                passwordBox_2.ToolTip = "";
+                passwordBox_2.Background = Brushes.Transparent;
+                textBoxEmail.ToolTip = "";
+                textBoxEmail.Background = Brushes.Transparent;
+
+                MessageBox.Show("Registration completed successfully");
+            }
+        }
     }
 }
